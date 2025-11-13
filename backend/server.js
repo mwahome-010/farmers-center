4fa01fe04296137538e6dd92e01e05e4d4039cbe
mwahome-010 <mwahome482@gmail.com>
2025-11-13 21:10:53 +0300

    minor search bar styling

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif|webp/;
+    const allowedTypes = /jpeg|jpg|png|webp/;
     const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = allowedTypes.test(file.mimetype);
 
